e83260d1994d372d2b7ad056bd064c364951bd09
danimolsen1988 <201808188@post.au.dk>
2020-02-18 22:06:08 +0100

    Added shadowfunktion and ADXLtest

--- ADXL345.cpp
+++ ADXL345.cpp
@@ -16,8 +16,8 @@
 * @param address The device ID on the bus.
  */
 
-ADXL345::ADXL345 (unsigned int bus, unsigned int address ):
-I2C(I2CBus, I2CAddress)
+ADXL345::ADXL345(unsigned int bus, unsigned int address) :
+	I2C(I2CBus, I2CAddress)
 {
 	this->I2CAddress = address;
 	this->I2CBus = bus;
@@ -33,7 +33,7 @@ I2C(I2CBus, I2CAddress)
 
 }
 
-ADXL345::~ADXL345 ( )
+ADXL345::~ADXL345()
 {
 	// TODO Auto-generated destructor stub
 }
@@ -43,21 +43,21 @@ ADXL345::~ADXL345 ( )
  * @param msb an unsigned character that contains the most significant byte
  * @param lsb an unsigned character that contains the least significant byte
  */
-short ADXL345::combineRegisters(unsigned char msb, unsigned char lsb){
-   //shift the MSB left by 8 bits and OR with LSB
-   return ((short)msb<<8)|(short)lsb;
+short ADXL345::combineRegisters(unsigned char msb, unsigned char lsb) {
+	//shift the MSB left by 8 bits and OR with LSB
+	return ((short)msb << 8) | (short)lsb;
 }
 /**
  *  @breif used to update the DATA_FORMAT register
  * @return 0 if the register is updated successfully and 1 on failure
  */
-int ADXL345::updateRegisters(){
-   //update the DATA_FORMAT register
-   char data_format = 0x00;  //+/- 2g with normal resolution
-   //Full_resolution is the 3rd LSB
-   data_format = data_format|((this->resolution)<<3);
-   data_format = data_format|this->range; // 1st and 2nd LSB therefore no shift
-   return this->writeRegister(DATA_FORMAT, data_format);
+int ADXL345::updateRegisters() {
+	//update the DATA_FORMAT register
+	char data_format = 0x00;  //+/- 2g with normal resolution
+	//Full_resolution is the 3rd LSB
+	data_format = data_format | ((this->resolution) << 3);
+	data_format = data_format | this->range; // 1st and 2nd LSB therefore no shift
+	return this->writeRegister(DATA_FORMAT, data_format);
 }
 
 /**
@@ -68,7 +68,7 @@ int ADXL345::updateRegisters(){
 int ADXL345::setRange(ADXL345::RANGE range)
 {
 	this->range = range;
-	if(updateRegisters() == 1) {
+	if (updateRegisters() == 1) {
 		perror("ADXL345: Failed write Range to register\n");
 		return 1;
 	}
@@ -82,7 +82,7 @@ int ADXL345::setRange(ADXL345::RANGE range)
 int ADXL345::setResolution(ADXL345::RESOLUTION resulution)
 {
 	this->resolution = resulution;
-	if(updateRegisters() == 1) {
+	if (updateRegisters() == 1) {
 		perror("ADXL345: Failed write resulotion to register\n");
 		return 1;
 	}
@@ -96,7 +96,7 @@ int ADXL345::setResolution(ADXL345::RESOLUTION resulution)
 int ADXL345::setBWrate(ADXL345::BAUD baud)
 {
 	this->baud = baud;
-	if(this->writeRegister(BW_RATE,baud) == 1) {
+	if (this->writeRegister(BW_RATE, baud) == 1) {
 		perror("ADXL345: Failed write Baud rate to register\n");
 		return 1;
 	}
@@ -107,17 +107,17 @@ int ADXL345::setBWrate(ADXL345::BAUD baud)
  * @breif Reads sensor state, by reading register 0x32-0x37 and combining 2 registers
  * @return 0 if the registers are successfully read and -1 if the device ID is incorrect.
  */
-int ADXL345::ReadSensorState(){
+int ADXL345::ReadSensorState() {
 	this->registers = this->readRegisters(BUFFER_SIZE, 0x00);
-	if(*this->registers!=0xe5){
+	if (*this->registers != 0xe5) {
 		perror("ADXL345: Failure Condition - Sensor ID not Verified");
 		return -1;
 	}
-	this->accelX = this->combineRegisters(*(registers+DATAX1), *(registers+DATAX0));
-	this->accelY = this->combineRegisters(*(registers+DATAY1), *(registers+DATAY0));
-	this->accelZ = this->combineRegisters(*(registers+DATAZ1), *(registers+DATAZ0));
-	this->resolution = (ADXL345::RESOLUTION) (((*(registers+DATA_FORMAT))&0x08)>>3);
-	this->range = (ADXL345::RANGE) ((*(registers+DATA_FORMAT))&0x03);
+	this->accelX = this->combineRegisters(*(registers + DATAX1), *(registers + DATAX0));
+	this->accelY = this->combineRegisters(*(registers + DATAY1), *(registers + DATAY0));
+	this->accelZ = this->combineRegisters(*(registers + DATAZ1), *(registers + DATAZ0));
+	this->resolution = (ADXL345::RESOLUTION) (((*(registers + DATA_FORMAT)) & 0x08) >> 3);
+	this->range = (ADXL345::RANGE) ((*(registers + DATA_FORMAT)) & 0x03);
 	return 0;
 }
 
@@ -127,15 +127,15 @@ int ADXL345::ReadSensorState(){
  * @return 0 if the file is opened correctly
  */
 int ADXL345::OpenOutFile() {
-    //open output file
-    this->outFile.open(OUT_FILE, ios::out | ios::trunc);
-    if(!this->outFile.is_open()){
-        return 1;
-    }
+	//open output file
+	this->outFile.open(OUT_FILE, ios::out | ios::trunc);
+	if (!this->outFile.is_open()) {
+		return 1;
+	}
 
-    //write header for file
-    this->outFile << "x,y,z" << endl;
-    return 0;
+	//write header for file
+	this->outFile << "x,y,z" << endl;
+	return 0;
 }
 
 /**
@@ -143,9 +143,16 @@ int ADXL345::OpenOutFile() {
  * @return 0 if file is closed
  */
 int ADXL345::CloseOutFile() {
-    //close output file
-    this->outFile.close();
-    return 0;
+	//close output file
+	//flush buffer before file is closed
+	//check for bad bit before closing.
+	this->outFile.flush();
+	if (this->outFile.bad()) {
+		this->outFile.close();
+		return 1;
+	}
+	this->outFile.close();
+	return 0;
 }
 
 /**
@@ -153,10 +160,26 @@ int ADXL345::CloseOutFile() {
  * @return 0 if write is successful
  */
 int ADXL345::WriteDataToFile(short x, short y, short z) {
-    //write line to output file
-    this->outFile << x << "," << y << "," << z << endl;
+	//write line to output file
+	//check is bad bit is set. this bit is only set when there is
+	//an actual write error, not guaranteed to happen before an actual
+	// write is performed.
+	if (this->outFile) {
+		this->outFile << x << "," << y << "," << z << endl;
+	}
+	if (this->outFile.bad()) {
+		return 1;
+	}
+	return 0;
+}
 
-    //TODO
-    // implement write verification
-    return 0;
+int ADXL345::WriteDataToFile(ostream os, short x, short y, short z) {
+	//function for testing write funcktion - boost variant
+	if (os) {
+		os << x << "," << y << "," << z << endl;
+	}
+	if (os.bad()) {
+		return 1;
+	}
+	return 0;
 }
--- ADXL345.h
+++ ADXL345.h
@@ -60,61 +60,62 @@ using namespace std;
  *
  */
 
-class ADXL345:protected I2C,public ISensor
+class ADXL345 :protected I2C, public ISensor
 {
-	public:
-		// An enumeration to define the gravity range of the sensor
-		enum RANGE {
-			PLUSMINUS_2_G 		= 0,// plus/minus 2g
-			PLUSMINUS_4_G 		= 1,// plus/minus 4g
-			PLUSMINUS_8_G 		= 2,// plus/minus 8g
-			PLUSMINUS_16_G 		= 3 // plus/minus 16g
-		};
-		// The resolution of the sensor. High is only available in +/- 16g range.
-		enum RESOLUTION {
-			NORMAL = 0,//!< NORMAL 10-bit resolution
-			HIGH = 1   //!< HIGH 13-bit resolution
-		};
-		// an enumeration to define Baud bandwich and output rate.
-		enum BAUD {
-			BANDWITH_3f125_Hz		= 6,	// BW 3.125 Hz Outputrate 2*BW Hz
-			BANDWITH_6f25_Hz		= 7,	// BW 6.25 Hz Outputrate 2*BW Hz
-			BANDWITH_12f5_Hz		= 8,	// BW 12.5 Hz Outputrate 2*BW Hz
-			BANDWITH_25_Hz			= 9,	// BW 25 Hz Outputrate 2*BW Hz
-			BANDWITH_50_Hz			= 10,	// BW 50 Hz Outputrate 2*BW Hz
-			BANDWITH_100_Hz			= 11,	// BW 100 Hz Outputrate 2*BW Hz
-			BANDWITH_200_Hz			= 12,	// BW 200 Hz Outputrate 2*BW Hz
-			BANDWITH_400_Hz			= 13,	// BW 400 Hz Outputrate 2*BW Hz
-			BANDWITH_800_Hz			= 14,	// BW 800 Hz Outputrate 2*BW Hz
-			BANDWITH_1600_Hz		= 15	// BW 1600 Hz Outputrate 2*BW Hz
-		};
+public:
+	// An enumeration to define the gravity range of the sensor
+	enum RANGE {
+		PLUSMINUS_2_G = 0,// plus/minus 2g
+		PLUSMINUS_4_G = 1,// plus/minus 4g
+		PLUSMINUS_8_G = 2,// plus/minus 8g
+		PLUSMINUS_16_G = 3 // plus/minus 16g
+	};
+	// The resolution of the sensor. High is only available in +/- 16g range.
+	enum RESOLUTION {
+		NORMAL = 0,//!< NORMAL 10-bit resolution
+		HIGH = 1   //!< HIGH 13-bit resolution
+	};
+	// an enumeration to define Baud bandwich and output rate.
+	enum BAUD {
+		BANDWITH_3f125_Hz = 6,	// BW 3.125 Hz Outputrate 2*BW Hz
+		BANDWITH_6f25_Hz = 7,	// BW 6.25 Hz Outputrate 2*BW Hz
+		BANDWITH_12f5_Hz = 8,	// BW 12.5 Hz Outputrate 2*BW Hz
+		BANDWITH_25_Hz = 9,	// BW 25 Hz Outputrate 2*BW Hz
+		BANDWITH_50_Hz = 10,	// BW 50 Hz Outputrate 2*BW Hz
+		BANDWITH_100_Hz = 11,	// BW 100 Hz Outputrate 2*BW Hz
+		BANDWITH_200_Hz = 12,	// BW 200 Hz Outputrate 2*BW Hz
+		BANDWITH_400_Hz = 13,	// BW 400 Hz Outputrate 2*BW Hz
+		BANDWITH_800_Hz = 14,	// BW 800 Hz Outputrate 2*BW Hz
+		BANDWITH_1600_Hz = 15	// BW 1600 Hz Outputrate 2*BW Hz
+	};
 
-		ADXL345 (unsigned int bus, unsigned int address=0x53 );
-		int setRange(ADXL345::RANGE range);
-		int setResolution(ADXL345::RESOLUTION resolution);
-		int setBWrate(ADXL345::BAUD baud);
-		int updateRegisters();
-		virtual ~ADXL345 ( );
-		ADXL345::RANGE getRange() { return this->range; };
-		ADXL345::RESOLUTION getResolution() {return this->resolution; };
-		ADXL345::BAUD getBaud() {return this->baud; };
-		short getaccelX() { return this->accelX; };
-		short getaccelY() { return this->accelY; };
-		short getaccelZ() { return this->accelZ; };
-        int WriteDataToFile(short x, short y, short z) override;
-        int OpenOutFile() override;
-        int CloseOutFile() override;
-        int ReadSensorState() override;
+	ADXL345(unsigned int bus, unsigned int address = 0x53);
+	int setRange(ADXL345::RANGE range);
+	int setResolution(ADXL345::RESOLUTION resolution);
+	int setBWrate(ADXL345::BAUD baud);
+	int updateRegisters();
+	virtual ~ADXL345();
+	ADXL345::RANGE getRange() { return this->range; };
+	ADXL345::RESOLUTION getResolution() { return this->resolution; };
+	ADXL345::BAUD getBaud() { return this->baud; };
+	short getaccelX() { return this->accelX; };
+	short getaccelY() { return this->accelY; };
+	short getaccelZ() { return this->accelZ; };
+	int WriteDataToFile(short x, short y, short z) override;
+	int WriteDataToFile(ostream os, short x, short y, short z);
+	int OpenOutFile() override;
+	int CloseOutFile() override;
+	int ReadSensorState() override;
 
-	private:
-		short combineRegisters(unsigned char msb, unsigned char lsb);
-		short accelX,accelY,accelZ;
-		unsigned int I2CAddress,I2CBus;
-		unsigned char * registers;
-		ADXL345::RANGE range;
-		ADXL345::RESOLUTION resolution;
-		ADXL345::BAUD baud;
-		ofstream outFile;
+private:
+	short combineRegisters(unsigned char msb, unsigned char lsb);
+	short accelX, accelY, accelZ;
+	unsigned int I2CAddress, I2CBus;
+	unsigned char * registers;
+	ADXL345::RANGE range;
+	ADXL345::RESOLUTION resolution;
+	ADXL345::BAUD baud;
+	ofstream outFile;
 
 
 
